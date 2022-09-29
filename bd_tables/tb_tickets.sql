84b6d40f38c8fd2e101efb292dbc6e43adbf6aa1
HilariWeb <hilariweb@gmail.com>
2022-09-29 15:42:08 -0400

    incorporamos la tabla tickets

---
+++ bd_tables/tb_tickets.sql
@@ -0,0 +1,16 @@
+
+
+CREATE TABLE tb_tickets(
+    id_ticket               INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre_cliente          VARCHAR (255) NULL,
+    nit_ci                  VARCHAR (255) NULL,
+    cuviculo                VARCHAR (255) NULL,
+    fecha_ingreso           VARCHAR (255) NULL,
+    hora_ingreso            VARCHAR (255) NULL,
+    user_sesion             VARCHAR (255) NULL,
+
+    fyh_creacion            DATETIME        NULL,
+    fyh_actualizacion       DATETIME        NULL,
+    fyh_eliminacion         DATETIME        NULL,
+    estado                  VARCHAR (10)
+);
